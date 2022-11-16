649cb81a00b66c5e6581409325fcb2224133c4bf
atron-games <aaron.cain@edu.sait.ca>
2022-11-16 05:16:42 -0700

    polymorphism and textures fix
    added polymorphism properties to the kitten class.
    
    fixed cat material so shadows are properly cast on the ground.

--- Assets/Scripts/Kitten.cs
+++ Assets/Scripts/Kitten.cs
@@ -16,4 +16,19 @@ public class Kitten : Animal
         PassInputs();
     }
 
+    public override void Move(float v, float h)
+    {
+        float speed = 6;
+        float rotationSpeed = 60;
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * v);
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * h);
+    }
+
+    public override void Jump()
+    {
+        float jumpForce = 4;
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
 }
